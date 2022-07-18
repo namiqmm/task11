1497f1dc4ef3f02336a96aa4d6a354f123cc03f6
namiqmm <107476714+namiqmm@users.noreply.github.com>
2022-07-18 19:14:55 +0400

    Update Program.cs

--- ConsoleApp13/ConsoleApp13/Program.cs
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -8,8 +8,18 @@ namespace ConsoleApp13
         {            
             User user1 = new User("Eli","Kazimov","4K12",Month.April);
             Console.WriteLine(user1.GetDetails());
-            User user2 = new User("Namiq", "Mamedov", "8KNQW2", Month.October);\
-            Console.WriteLine(user2.GetDetails());
+            User user2 = new User("Namiq", "Mamedov", "8KNQW2", Month.October);
+            Console.WriteLine(user2.GetDetails());            
+            User user3 = new User("Resad", "Ibrahimov, "42K3", Month.February);
+            Console.WriteLine(user3.GetDetails());
+            User user4 = new User("Aqil", "Huseynov", "DK3F", Month.May);
+            Console.WriteLine(user4.GetDetails());
+            User user5 = new User("Vaqif", "Aliyev", "30J1", Month.July);
+            Console.WriteLine(user5.GetDetails());                      
+                                  
+                                  
+                                  
+                                  
                         
         }
 
